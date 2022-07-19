6111377148c81cd813e30c0c8fa04949ae23e1f5
farhajsimform <farhaj.h@simformsolutions.com>
2022-07-19 11:28:50 +0530

    fixed untracked files

--- dist/user/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '@prisma/client';
-import { EditUserDto } from './dto';
-import { UserService } from './user.service';
-export declare class UserController {
-    private userService;
-    constructor(userService: UserService);
-    getMe(user: User): User;
-    editUser(userId: number, dto: EditUserDto): Promise<User>;
-}
